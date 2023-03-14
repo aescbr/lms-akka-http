f78a303d700cb5c1fbaba7671b53898649a6818d
aescbr <aescobar@applaudostudios.com>
2023-03-14 10:40:20 -0600

    add config to data access

--- src/main/scala/com/applaudo/akkalms/databseDAO/PostgresDAO.scala
+++ src/main/scala/com/applaudo/akkalms/databseDAO/PostgresDAO.scala
@@ -1,13 +1,17 @@
 package com.applaudo.akkalms.databseDAO
 
 import cats.effect.IO
+import com.typesafe.config.{Config, ConfigFactory}
 import doobie.Transactor
 
 object PostgresDAO {
+  val config: Config = ConfigFactory.load().getConfig("postgresql")
+
+  val url: String = config.getString("url")
+  val username: String = config.getString("username")
+  val password: String = config.getString("password")
+
  def xa = Transactor.fromDriverManager[IO](
-    "org.postgresql.Driver",
-    "jdbc:postgresql://localhost:5432/latest",
-    "latest",
-    "latest"
+    "org.postgresql.Driver", url, username, password
   )
 }
