a025e9bff0fc74754a92e7ec86790b6890311354
aescbr <aescobar@applaudostudios.com>
2023-03-22 15:11:15 -0600

    refactor normalizer

--- src/main/scala/com/applaudo/akkalms/actors/ProgressNormalizer.scala
+++ src/main/scala/com/applaudo/akkalms/actors/ProgressNormalizer.scala
@@ -2,14 +2,29 @@ package com.applaudo.akkalms.actors
 
 import akka.actor.{Actor, ActorLogging}
 import com.applaudo.akkalms.actors.ProgramManager.ProgressModel
+import com.applaudo.akkalms.actors.ProgressNormalizer.{FailedInsert, SuccessInsert}
 import com.applaudo.akkalms.databseDAO.ProgressQueries
+import org.postgresql.util.PSQLException
 
+object ProgressNormalizer{
+  trait ProgressNormalizerResponse
+  case class SuccessInsert(inserted: Int) extends ProgressNormalizerResponse
+  case class FailedInsert(reason: String) extends ProgressNormalizerResponse
+
+}
 
 class ProgressNormalizer extends Actor with ActorLogging {
 
   override def receive: Receive = {
     case ProgressModel(programId, courseId, contentId, userId, completed, total) =>
       //save progress to postgresql
-      ProgressQueries.insert(ProgressModel(programId, courseId, contentId, userId, completed, total))
+      var response =0
+      try{
+         response = ProgressQueries.insert(ProgressModel(programId, courseId, contentId, userId, completed, total))
+         sender() ! SuccessInsert(response)
+      }catch {
+        case e: PSQLException =>
+          sender() ! FailedInsert("unable to save progress due to: "+e.getMessage)
+      }
   }
 }
