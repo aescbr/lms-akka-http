01c950b10b637f976127ad657868cc75ece38955
aescbr <aescobar@applaudostudios.com>
2023-03-22 15:04:49 -0600

    refactor normalizer

--- src/main/scala/com/applaudo/akkalms/actors/LatestManager.scala
+++ src/main/scala/com/applaudo/akkalms/actors/LatestManager.scala
@@ -3,9 +3,8 @@ package com.applaudo.akkalms.actors
 import akka.actor.{Actor, ActorLogging, ActorRef}
 import akka.pattern.ask
 import akka.util.Timeout
-import com.applaudo.akkalms.actors.ProgramManager.{ProgramManagerTag, ProgressModel}
+import com.applaudo.akkalms.actors.ProgramManager.ProgressModel
 import com.softwaremill.macwire.akkasupport.wireActor
-import com.softwaremill.tagging.@@
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
@@ -14,37 +13,29 @@ object LatestManager {
   trait LatestManagerTag
 }
 
-class LatestManager(programManager: ActorRef @@ ProgramManagerTag) extends Actor with ActorLogging{
-  import ProgressActor._
+class LatestManager extends Actor with ActorLogging{
+  import ProgressNormalizer._
 
-  implicit val timeout = Timeout(3 seconds)
+  implicit val timeout: Timeout = Timeout(3 seconds)
 
   override def receive: Receive = {
-    case SaveProgress(programId, courseId, contentId, userId, completed) =>
-      val saveProgress = SaveProgress(programId, courseId, contentId, userId, completed)
+    case ProgressModel(programId, courseId, contentId, userId, completed, total) =>
+      log.info("here")
+      val progress = ProgressModel(programId, courseId, contentId, userId, completed, total)
 
-      //validate and get progress model from ProgramManager
-      val progressModel = (programManager ? saveProgress)
-        .mapTo[Option[ProgressModel]]
       //find or init child normalizer
       val normalizer = getProgressNormalizerChild("progress-normalizer")
-      // send message
-     progressModel.map{
-       case Some(progress) =>
-         log.info(progress.toString)
-         normalizer ! progress
-       case None =>
-         log.error(s"Invalid progress request information $saveProgress")
-     }
+      val result = (normalizer ? progress).mapTo[ProgressNormalizerResponse]
+      result.map{r =>
+        log.info(r.toString)
+      }
   }
 
   def getProgressNormalizerChild(name: String): ActorRef = {
     context.child(name) match {
       case Some(child) =>
-        log.info("actor found")
         child
       case None =>
-        log.warning("actor NOT found")
          wireActor[ProgressNormalizer](name)
     }
   }
