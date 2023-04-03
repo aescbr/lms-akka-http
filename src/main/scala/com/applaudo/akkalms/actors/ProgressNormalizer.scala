eda1e478010352eecc341a15a7997c2d4c8b99da
aescbr <aescobar@applaudostudios.com>
2023-04-03 10:13:13 -0600

    refactor progress normalizer

--- src/main/scala/com/applaudo/akkalms/actors/ProgressNormalizer.scala
+++ src/main/scala/com/applaudo/akkalms/actors/ProgressNormalizer.scala
@@ -1,31 +1,37 @@
 package com.applaudo.akkalms.actors
 
-import akka.actor.{Actor, ActorLogging}
+import akka.actor.{Actor, ActorLogging, ActorRef}
 import com.applaudo.akkalms.actors.ProgramManager.ProgressModel
-import com.applaudo.akkalms.actors.ProgressNormalizer.{FailedInsert, SuccessInsert}
+import com.applaudo.akkalms.actors.ProgressNormalizer.{FailedInsert, SaveState, SuccessInsert}
 import com.applaudo.akkalms.databseDAO.ProgressQueries
-import org.postgresql.util.PSQLException
+
+import java.sql.SQLException
 
 object ProgressNormalizer{
   sealed trait ProgressNormalizerResponse
-  case class SuccessInsert(progress: ProgressModel, insertedRows: Int) extends ProgressNormalizerResponse
-  case class FailedInsert(progress: ProgressModel, reason: String) extends ProgressNormalizerResponse
+  final case class SuccessInsert(progress: ProgressModel, insertedRows: Int) extends ProgressNormalizerResponse
+  final case class FailedInsert(progress: ProgressModel) extends ProgressNormalizerResponse
+
+  case class SaveState(model: ProgressModel, replyTo: ActorRef)
 
 }
 
-class ProgressNormalizer extends Actor with ActorLogging {
+class ProgressNormalizer(queries: ProgressQueries) extends Actor with ActorLogging {
 
-  override def receive: Receive = {
-    case ProgressModel(programId, courseId, contentId, userId, completed, total) =>
+    override def receive: Receive = {
+    case state : SaveState =>
       //save progress to postgresql
-
-      val progress = ProgressModel(programId, courseId, contentId, userId, completed, total)
       try{
-         val response = ProgressQueries.insert(ProgressModel(programId, courseId, contentId, userId, completed, total))
-         sender() ! SuccessInsert(progress, response)
+         val response = insertQuery(state.model)
+         state.replyTo ! SuccessInsert(state.model, response)
       }catch {
-        case e: PSQLException =>
-          sender() ! FailedInsert(progress, s"unable to save progress due to: ${e.getMessage}")
+        case e: Exception =>
+          log.info(s"unable to save progress due to: ${e.getMessage}")
+          state.replyTo ! FailedInsert(state.model)
       }
+  }
+
+  def insertQuery(model: ProgressModel): Int = {
+    queries.insert(model)
   }
 }
